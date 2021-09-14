bfd0066b4d1c92ca780067aa9533f56b6b959206
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2021-09-14 14:46:00 +0700

    perbaikan agar aman disimpan pake storage

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -27,12 +27,12 @@ class ImageUploadController extends Controller
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
-        $imageName = time() . '.' . $request->image->extension();
-
-        $request->image->move(public_path('images'), $imageName);
-
+        if (!empty($request->image)) {
+            $imageName = $request->image->getClientOriginalName();
+            $request->image->move(public_path('storage/produk-image'), $imageName);
+        }
+        $input['image'] = $imageName;
         /* Store $imageName name in DATABASE from HERE */
-
         return back()
             ->with('success', 'You have successfully upload image.')
             ->with('image', $imageName);
